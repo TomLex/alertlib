b6306807944b938213e90fd6b6ce8b945b6d77fd
Tomas Varga <tomas.varga@skypicker.com>
2015-03-31 20:47:31 +0200

    alert on email via mandrill

--- alertlib/__init__.py
+++ alertlib/__init__.py
@@ -5,6 +5,41 @@ from functools import wraps
 
 from pyslack import SlackClient
 
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+
+
+########################################  EMAIL  ########################################
+def send_to_email(mandrill_login, email_from='alertlib@skypicker.com', email_to=[], subject='', message='', list_of_files=[]):
+    # email_to must be list
+    smtp = smtplib.SMTP("smtp.mandrillapp.com",587)
+    smtp.starttls()
+    smtp.login(*mandrill_login)
+
+    mpart = MIMEMultipart()
+    mpart.set_charset("utf-8")
+    mpart["From"] = email_from
+    mpart["Subject"] = subject
+
+    mpart.attach(MIMEText(message, 'plain'))
+
+    for f in list_of_files:
+        fpart = MIMEBase('application', "octet-stream")
+        fpart.set_payload( open(f,"rb").read() )
+        Encoders.encode_base64(fpart)
+        fpart.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+        mpart.attach(fpart)
+                
+    for to in email_to:
+        mpart["To"] = to
+        smtp.sendmail(mpart["From"], to, mpart.as_string())
+
+    smtp.close()
+
+
 
 ########################################  SLACK  ########################################
 
